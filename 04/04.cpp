eb7a6b49c0b7856d4b2da9e9158a91cce2fc9fa0
lSoleyl <lSoleyl+github@gmail.com>
2025-01-23 21:26:02 +0100

    Day 4 Part 2 completed

--- 04/04.cpp
+++ 04/04.cpp
@@ -3,6 +3,7 @@
 #include <string_view>
 #include <set>
 #include <algorithm>
+#include <numeric>
 #include <ranges>
 #include <regex>
 
@@ -18,28 +19,28 @@ struct Card {
     std::smatch match;
     std::regex_match(line, match, lineRegex);
     cardNumber = std::stoi(match[1].str());
-    
-    winningNumbers = regex::iter(std::string_view(match[2].first, match[2].second), numberRegex)
+
+    auto winningNumbers = regex::iter(std::string_view(match[2].first, match[2].second), numberRegex)
       | std::views::transform([](auto& match) { return std::stoi(match[0].str()); })
       | std::ranges::to<std::set>();
 
-    ownNumbers = regex::iter(std::string_view(match[3].first, match[4].second), numberRegex)
+    auto ownNumbers = regex::iter(std::string_view(match[3].first, match[4].second), numberRegex)
       | std::views::transform([](auto& match) { return std::stoi(match[0].str()); })
       | std::ranges::to<std::vector>();
+
+    matchCount = std::ranges::count_if(ownNumbers, [&](int number) { return winningNumbers.contains(number); });
   }
 
+
   int value() const {
-    int wonCount = std::ranges::count_if(ownNumbers, [=](int number) { return winningNumbers.contains(number); });
-    return wonCount ? 1 << (wonCount - 1) : 0;
+    return matchCount ? 1 << (matchCount - 1) : 0;
   }
 
 
   int cardNumber;
-  std::set<int> winningNumbers;
-  std::vector<int> ownNumbers;
+  int matchCount; // we only need the match count for both parts
+  int copiesForCard = 0; // the number of copies we receive for this card
 };
-
-
 
 
 int main()
@@ -47,12 +48,33 @@ int main()
   common::Time t;
 
   int part1 = 0;
+  std::vector<Card> cards;
+
+  // Part 1
   for (auto line : stream::lines(std::ifstream("input.txt"))) {
-    Card card(std::move(line));
-    part1 += card.value();
+    cards.emplace_back(std::move(line));
+    part1 += cards.back().value();
   }
 
+  // Part 2: set the number of copies by iterating backwards (important to avoid costly recursion and reevaluation)
+  for (int i = cards.size() - 1; i >= 0; --i) {
+    auto& card = cards[i];
+    // Add the count of immediately following copies to the count (std::min to ensure we don't copy past the last card)
+    auto followingCopies = std::min(card.matchCount, (static_cast<int>(cards.size()) - 1) - i);
+    card.copiesForCard += followingCopies;
 
-  std::cout << "Part1: " << part1 << "\n"; // 21213
+    // Now for each of the following copies add up their copy value fo this card, because we will evaluate them too
+    card.copiesForCard += std::ranges::fold_left(
+      std::views::iota(0, followingCopies) | std::views::transform([&](int offset) { return cards[i + offset + 1].copiesForCard; }),
+      0, std::plus{}
+    );
+  }
+
+  // Count up the copiesForCard (+1 for the card itself)
+  auto part2 = std::ranges::fold_left(cards | std::views::transform([](const Card& card) { return card.copiesForCard + 1; }), 0, std::plus{});
+
+
+  std::cout << "Part 1: " << part1 << "\n"; // 21213
+  std::cout << "Part 2: " << part2 << "\n"; // 8549735
   std::cout << t;
 }
